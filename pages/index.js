d5667e7db02370657c4c44412e66524d953f4c0b
cuong-ich-truong <cuongichtruong@gmail.com>
2019-09-09 22:11:26 -0700

    Add Amp to index page

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,40 @@
-import Layout from '../components/MyLayout';
-import Link from 'next/link';
+import Layout from "../components/MyLayout";
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
+import { useAmp } from "next/amp";
 
-function getPosts() {
-  return [
-    { id: 'hello-nextjs', title: 'Hello Next.js' },
-    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
-    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
-  ];
-}
+export const config = { amp: "hybrid" };
 
-const PostLink = ({ post }) => (
-  <li>
-    <Link href="/p/[id]" as={`/p/${post.id}`}>
-      <a>{post.title}</a>
-    </Link>
-    <style jsx>{`
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
-
-      a {
-        text-decoration: none;
-        color: blue;
-        font-family: 'Arial';
-      }
-
-      a:hover {
-        opacity: 0.6;
-      }
-    `}</style>
-  </li>
-);
-
-export default function Blog() {
+const Index = props => {
+  const isAmp = useAmp();
   return (
     <Layout>
-      <h1>My Blog</h1>
+      <h1>Batman TV Shows</h1>
       <ul>
-        {getPosts().map(post => (
-          <PostLink key={post.id} post={post} />
+        {props.shows.map(show => (
+          <li key={show.id}>
+            <Link href="/show/[id]" as={`/show/${show.id}`}>
+              <a>{show.name}</a>
+            </Link>
+          </li>
         ))}
       </ul>
-      <style jsx>{`
-        h1,
-        a {
-          font-family: 'Arial';
-        }
-
-        ul {
-          padding: 0;
-        }
-
-        li {
-          list-style: none;
-          margin: 5px 0;
-        }
-
-        a {
-          text-decoration: none;
-          color: blue;
-        }
-
-        a:hover {
-          opacity: 0.6;
-        }
-      `}</style>
+      <p>
+        Welcome to the {isAmp ? "AMP" : "normal"} version of the Index page!!
+      </p>
     </Layout>
   );
-}
+};
+
+Index.getInitialProps = async function() {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Index;
